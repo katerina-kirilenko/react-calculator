9a21c30dff4ce655661180e7a5abd564734d8953
Elaterina Kirilenko <katerina.sanway@gmail.com>
2020-10-02 18:20:55 +0300

    add calculated result, refactoring

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import faker from 'faker';
 import Header from '@/components/blocks/Header';
 import Rows from '@/components/blocks/Rows';
@@ -7,6 +7,17 @@ import { PLUS } from '@/constants/constants';
 
 const App = () => {
   const [rows, setRows] = useState([]);
+  const [result, setResult] = useState(0);
+
+  useEffect(() => {
+    const activeRows = rows.filter((row) => row.disable === false);
+    const summary = activeRows.reduce((sum, current) => {
+      const currentValue = `${current.signSelected}${current.value}`;
+      const num = Number(current.value >= 0 ? currentValue : currentValue.slice(2));
+      return sum + num;
+    }, 0);
+    setResult(summary);
+  }, [rows]);
 
   const handlerRowAdd = useCallback(() => {
     const newRow = {
@@ -16,10 +27,10 @@ const App = () => {
       disable: false,
     };
     setRows((prevRows) => [...prevRows, newRow]);
-  }, [rows]);
+  }, []);
 
   const handlerRowDelete = useCallback(
-    (id) => {
+    (id) => () => {
       const newRows = rows.filter((row) => row.id !== id);
       setRows(newRows);
     },
@@ -27,7 +38,7 @@ const App = () => {
   );
 
   const handlerToggleDisable = useCallback(
-    (id) => {
+    (id) => () => {
       const newRows = rows.map((row) => {
         if (row.id === id) {
           return {
@@ -42,16 +53,49 @@ const App = () => {
     [rows],
   );
 
-  // temp
-  const inputsCalculate = useCallback(() => {
-    const activeRows = rows.filter((row) => row.disable === false);
-  }, [rows]);
+  const handleChangeInputs = useCallback(
+    (id) => (event) => {
+      const updateRowsValue = rows.map((row) => {
+        if (row.id === id) {
+          return {
+            ...row,
+            value: event.target.value,
+          };
+        }
+        return row;
+      });
+      setRows(updateRowsValue);
+    },
+    [rows],
+  );
+
+  const handleChangeSelect = useCallback(
+    (id) => (event) => {
+      const updateSignSelected = rows.map((row) => {
+        if (row.id === id) {
+          return {
+            ...row,
+            signSelected: event.target.value,
+          };
+        }
+        return row;
+      });
+      setRows(updateSignSelected);
+    },
+    [rows],
+  );
 
   return (
     <div className="container">
       <Header onRowAdd={handlerRowAdd} />
-      <Rows rows={rows} onRowDelete={handlerRowDelete} onToggleDisable={handlerToggleDisable} />
-      <Result result={0} />
+      <Rows
+        rows={rows}
+        onRowDelete={handlerRowDelete}
+        onToggleDisable={handlerToggleDisable}
+        onChangeInput={handleChangeInputs}
+        onChangeSelect={handleChangeSelect}
+      />
+      <Result result={result} />
     </div>
   );
 };
--- src/components/blocks/Row/component.jsx
+++ src/components/blocks/Row/component.jsx
@@ -2,23 +2,26 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { PLUS, MINUS } from '@/constants/constants';
 
-const Row = ({ row: { value, disable }, onRowDelete, onToggleDisable }) => {
-  const onChangeInput = (event) => {
-    console.log(event.target.value);
-  };
-
+const Row = ({
+  row: { value, disable },
+  onRowDelete,
+  onToggleDisable,
+  onChangeInput,
+  onChangeSelect,
+}) => {
   return (
     <>
-      <select className="form-control col-1 mr-3" disabled={disable}>
+      <select className="form-control col-1 mr-3" disabled={disable} onClick={onChangeSelect}>
         <option value={PLUS}>{PLUS}</option>
         <option value={MINUS}>{MINUS}</option>
       </select>
       <input
-        type="text"
+        type="number"
         className="form-control col-8"
         onChange={onChangeInput}
-        // value={value}
+        value={value}
         disabled={disable}
+        autoFocus
       />
       <div className="col-3 d-flex">
         <button type="button" className="btn btn-danger w-100 mr-3" onClick={onRowDelete}>
@@ -41,6 +44,8 @@ Row.propTypes = {
   }).isRequired,
   onRowDelete: PropTypes.func.isRequired,
   onToggleDisable: PropTypes.func.isRequired,
+  onChangeInput: PropTypes.func.isRequired,
+  onChangeSelect: PropTypes.func.isRequired,
 };
 
 export default Row;
